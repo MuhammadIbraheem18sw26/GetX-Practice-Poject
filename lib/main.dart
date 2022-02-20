f211f9db27d0f077659963018da15d1f25722431
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-02-21 00:37:21 +0500

    Routing done using GetX and also passing arguments from one screen to another Screen using GetX

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,24 @@ import 'content_page.dart';
 import 'my_detail_page.dart';
 import 'my_home_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'MyApp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: Container(color: Colors.white,),
+      initialRoute: MyHomePage.homepage,
+      getPages: [
+        GetPage(name: MyHomePage.homepage, page: () => MyHomePage()),
+        GetPage(name: ContentPage.contentPage, page: () => ContentPage()),
+        GetPage(name: DetailPage.detail, page: () => DetailPage()),
+      ],
     );
   }
 }
